5a9c3b78e97a6c231c128f3944b7340195ffa9d7
Ignacio Varela <ignaciovarela8@gmail.com>
2021-03-08 18:41:16 -0300

    fix: change test

--- test/test.py
+++ test/test.py
@@ -64,10 +64,11 @@ class TestDataAPI(unittest.TestCase):
         self.assertEqual(1, row['num_integer'])
 
     def test_data_api_types(self):
-        sql = "INSERT INTO aurora_data_api_test (id, a_name, doc, num_numeric, num_float, num_integer, ts, tz_notimezone, field_null, field_boolean) values (4,:name, :doc, 1.12345, 1.11,:num_integer, '1976-11-02 08:45:00 UTC', '2021-03-03 15:51:48.082288', :field_null, :field_boolean)"
+        sql = "INSERT INTO aurora_data_api_test (id, a_name, doc, num_numeric, num_float, num_integer, ts, tz_notimezone, field_null, field_boolean) values (4,:name, :doc, 1.12345, 1.11,:num_integer, '1976-11-02 08:45:00 UTC', '2021-03-03 15:51:48.082288', :field_null, :field_boolean) RETURNING id"
         parameters = ParameterBuilder().add_string("name", 'prueba').add_null('field_null').add_json('doc', {'key':'as'}).add_long('num_integer',1).add_boolean('field_boolean', True).build()
-        self.data_client.execute(sql, parameters, wrap_result=False)
-        parameters = ParameterBuilder().add_long("id", 4).build()
+        result = self.data_client.execute(sql, parameters)
+        result_map = GraphQLMapper(result.metadata).map(result.records)
+        parameters = ParameterBuilder().add_long("id", result_map[0]['id']).build()
         result = self.data_client.execute("select * from aurora_data_api_test where id =:id", parameters)
         row = GraphQLMapper(result.metadata).map(result.records)[0]
         self.assertEqual({'key':'as'}, row['doc'])
