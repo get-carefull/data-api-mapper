26ab16d2972fb14e0eb8a86a1768b35cc2989eb1
Flavio Oliveri <flavio.oliveri@gmail.com>
2021-05-26 23:03:21 -0400

    v0.2.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='data-api-mapper',
-    version='0.2.3',
+    version='0.2.4',
     url='https://github.com/get-carefull/data-api-mapper',
     author='Flavio Oliveri',
     author_email='flavio.oliveri@gmail.com',
